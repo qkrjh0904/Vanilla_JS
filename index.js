593e4ed2e4ddb987b327ba035f48767224a6225d
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-10 15:16:18 +0900

    [Add] Data type of JS

---
+++ index.js
@@ -0,0 +1,11 @@
+// coment of JS
+/*comment of JS*/
+/*
+const name = "jeongho";     //String
+const age = 27;             //const number
+let b = age-5;              //number
+const num = 11.1;           //const float num
+const yes_or_no = false;    //boolean
+
+console.log(name, age, b, num, yes_or_no);
+*/
