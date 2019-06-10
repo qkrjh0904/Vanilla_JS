943b995e24ee23c610e2c51747c0a1d3878c0d9d
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-10 15:39:19 +0900

    [Add] Object of JS

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const yesOrNo = false;    //boolean
 
 console.log(name, age, b, num, yesOrNo);
 */
+
+/* ---------- Arrays ----------
 const name = "Jeongho";
 const daysOfWeek = ["Mon","Tue", "Wed", "Thu", "Fri", "Sat", "Sun", name, true, 10, 10.1]; //지금과 같이 중간중간 구분으로 대문자를 넣는것을 camel space라고 한다. 낙타 등처럼.
 console.log(daysOfWeek);
+*/
+
+/* ---------- Object ----------
+const myInfo = {
+    name:"Jeongho",
+    age:27,
+    gender:"Male",
+    isHandsome:true,
+    favFood:["Chicken","Pizza", "Bugger"]
+}
+
+console.log(myInfo);
+*/
