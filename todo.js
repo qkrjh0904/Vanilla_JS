c565f9bcef904692ffeecfcc78b3973a813d2435
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-11 14:40:51 +0900

    [Add] to do list 삭제

--- todo.js
+++ todo.js
@@ -4,7 +4,18 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id != parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -13,9 +24,10 @@ function saveToDos(){
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = "❌";
     const span = document.createElement("span");
     const newId = toDos.length+1;
+    delBtn.innerHTML = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
